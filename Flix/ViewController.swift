aa642327f3cd8f37fa654a71b908262f8d0c32c5
Chase Carnaroli <chasecarnaroli@gmail.com>
2019-01-20 21:45:32 -0800

    Movie posters now load

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -37,6 +38,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.pictureView.af_setImage(withURL: posterUrl!)
         
         return cell
     }
